6dfef7c8b72da263793503a82b4e1c388bd8741f
aksharma-7 <52443790+aksharma-7@users.noreply.github.com>
2021-01-17 18:44:01 +0530

    Added Routes

--- server/server.js
+++ server/server.js
@@ -2,31 +2,41 @@ const express = require("express");
 require("dotenv").config();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const passport = require('passport');
+const path = require("path");
+const users = require("./routes/api/users");
 
-const PORT = process.env.PORT || 5000;
-const MONGO_URI = process.env.MONGO_URI;
-
-mongoose
-    .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('Successfully connected to DB'))
-    .catch(err => console.log(err.message))
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', (req, res)=> {
-    res.send('Hello World');
-});
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose
+.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => console.log('Successfully connected to DB'))
+.catch(err => console.log(err.message))
+
+mongoose.set("useFindAndModify", false);
+mongoose.Promise = global.Promise;
 
 
-app.post('/user', (req, res) => {
-    console.log(req.body);
-    res.send(req.body);
-})
+
+app.use(passport.initialize());
+require('./middleware/passport')(passport);
+app.use("/api/users", users);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+}
 
 
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is up and running on ${PORT}`);
 });
